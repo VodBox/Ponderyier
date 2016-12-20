88c62a938fa15bd64187e3e18bc3df655630ebe4
VodBox <dillon@vodbox.io>
2016-12-20 02:55:31 -0500

    Established Command Pipeline - Implemented Basic Ponder (MegaHAL) Functionality

--- index.js
+++ index.js
@@ -1,6 +1,130 @@
 var fs = require('fs');
+var WebSocket = require('ws');
 
-fs.readFile('./tokens/Pond.txt', 'utf8', function(err, data) {
-	console.log(err);
-	console.log(data);
+var username;
+var oauthToken;
+
+var channels;
+
+var callbacks = [];
+
+var commandRefs = {};
+
+var irc;
+
+fs.readFile('./config.json', 'utf8', function(err, data) {
+	if(err) {
+		console.log(err);
+		process.exit();
+	} else {
+		var result = JSON.parse(data);
+		username = result.username;
+		channels = result.channels;
+		if(result.token) {
+			oauthToken = result.token;
+			startPond();
+		} else {
+			fs.readFile(result.tokenLocation, 'utf8', function(error, dat) {
+				if(error) {
+					console.log(error);
+					process.exit();
+				} else {
+					oauthToken = dat;
+					startPond();
+				}
+			});
+		}
+	}
+});
+
+function startPond() {
+	irc = new WebSocket("wss://irc-ws.chat.twitch.tv/");
+	irc.on('open', function (event) {
+		irc.on('message', function(message) {
+			var data = message;
+			console.log(data);
+			if(data.trim() == "PING :tmi.twitch.tv") {
+				irc.send("PONG :tmi.twitch.tv");
+				console.log("PONGED");
+			}else {
+				var tags = {}
+				var tagPart = "";
+				tagPart = data.split(" ")[0];
+				if(tagPart.charAt(0) == "@") {
+					tagPart = tagPart.slice(1,tagPart.length);
+				}
+				keyValuePairs = tagPart.split(";");
+				for(var i = 0, pairs = keyValuePairs.length; i < pairs; ++i) {
+					var key = keyValuePairs[i].split("=")[0];
+					var value = keyValuePairs[i].replace(key + "=", "");
+					tags[key] = value;
+				}
+				if(data.match(/tmi.twitch.tv .+ \#\S+ \:/)) {
+					var contents = data.replace(tagPart + " ", "").split(/\:(.+)/)[1].split(/\:(.+)/);
+					//console.log(contents);
+					if(contents[0].match(/tmi.twitch.tv (.+) \#\S+ /)) {
+						tags["type"] = contents[0].match(/tmi.twitch.tv (.+) \#\S+ /)[1];
+						tags["channel"] = contents[0].match(/tmi.twitch.tv .+ \#(\S+) /)[1];
+						tags["user"] = contents[0].split("!")[0];
+						tags["message"] = contents[1];
+					} else {
+						tags["message"] = "";
+					}
+					issueCallbacks(tags["type"], tags);
+					//console.log(tags);
+					//console.log(tags.channel + ": <" + (tags["display-name"] ? tags["display-name"] : tags["user"]) + "> " + tags["message"]);
+				}
+			}
+		});
+		irc.send('PASS ' + oauthToken);
+		irc.send('NICK ' + username);
+		irc.send('CAP REQ :twitch.tv/tags twitch.tv/commands twitch.tv/membership');
+		fs.readdir('./channels/', function(err, files) {
+			for(var i = 0, l = files.length; i < l; ++i) {
+				irc.send('JOIN #' + files[i].replace('.json', ''));
+				fs.readFile('./channels/' + files[i], function(error, response) {
+					var config = JSON.parse(response);
+					for(var x = 0, j = config.commands.length; x < j; ++x) {
+						if(!commandRefs[config.commands[x].command]) {
+							commandRefs[config.commands[x].command] = new require('./' + config.commands[x].command)();
+						}
+						commandRefs[config.commands[x].command].addInstance(config.channel, config.commands[x].config);
+					}
+				});
+			}
+		});
+	});
+}
+
+function on(type, callback) {
+	if(!callbacks[type]) {
+		callbacks[type] = [];
+	}
+	callbacks[type][callbacks[type].length] = callback;
+} 
+
+function issueCallbacks(type, data) {
+	if(callbacks[type]) {
+		for(var i = 0, l = callbacks[type].length; i < l; ++i) {
+			callbacks[type][i](data);
+		}
+	}
+	if(callbacks['all'] && type == "all") {
+		for(var i = 0, l = callbacks['all'].length; i < l; ++i) {
+			callbacks['all'][i](data);
+		}
+	}
+}
+
+var symbols = ['<', '>', '?', ',', "'", '='];
+
+on('PRIVMSG', function(data) {
+	console.log(data.channel + ": <" + (data["display-name"] ? data["display-name"] : data["user"]) + "> " + data["message"]);
+	for(var command in commandRefs) {
+		var result = commandRefs[command].runCommand(data);
+		if(result !== undefined) {
+			console.log(result);
+			irc.send('PRIVMSG #' + data["channel"] + ' :' + symbols[Math.floor(Math.random() * symbols.length)] + " - " + result);
+		}
+	}
 });
--- ponder.js
+++ ponder.js
@@ -1,3 +1,101 @@
-exports.runCommand = function(user, message, tags) {
+var hal = require('jsmegahal');
+var zlib = require('zlib');
+var fs = require('fs');
+
+var users = {};
+
+var messageStore = [];
+
+var megaHAL;
+
+module.exports = function() {
+	megaHAL = new hal();
+	fs.readFile('./messages.zip', function(err, res) {
+		if(err) {
+			console.log(err);
+		} else {
+			zlib.unzip(res, function(error, buffer) {
+				messageStore = buffer.toString().split('\n');
+				for(var i = 0, l = messageStore.length; i < l; ++i) {
+					this.megaHAL.add(messageStore[i]);
+				}
+			});
+		}
+	});
 	
-}; 
+	this.addInstance = function(user, config) {
+		users[user] = {};
+		if(config["unlimitedPonders"]) {
+			users[user].unlimitedPonders = config["unlimitedPonders"];
+		} else {
+			users[user].unlimitedPonders = [];
+		}
+		if(config["resetTime"]) {
+			users[user].resetTime = config["resetTime"];
+		} else {
+			users[user].resetTime = 1600; // GMT
+		}
+		if(config["messageInterval"]) {
+			users[user].messageInterval = config["messageInterval"];
+		} else {
+			users[user].messageInterval = 30;
+		}
+		if(config["helpInterval"]) {
+			users[user].helpInterval = config["helpInterval"];
+		} else {
+			users[user].helpInterval = 15;
+		}
+		if(config["countInterval"]) {
+			users[user].countInterval = config["countInterval"];
+		} else {
+			users[user].countInterval = 10;
+		}
+		if(config["ignores"]) {
+			users[user].ignores = config["ignores"];
+		} else {
+			users[user].ignores = [];
+		}
+		
+		users[user].messagesLeft = 0;
+		users[user].helpLeft = 0;
+		users[user].countLeft = 0;
+	};
+	
+	this.runCommand = function(tags) {
+		if(users[tags["channel"]]) {
+			users[tags["channel"]].messagesLeft--;
+			users[tags["channel"]].helpLeft--;
+			users[tags["channel"]].countLeft--;
+			if(tags["message"].startsWith('!ponder ')) {
+				if(users[tags["channel"]].unlimitedPonders.indexOf(tags["user"]) > -1) {
+					return megaHAL.getReplyFromSentence(tags["message"].replace('/\!ponder /', ''));
+				}else if(users[tags["channel"]].messagesLeft < 1) {
+					return megaHAL.getReplyFromSentence(tags["message"].replace('/\!ponder /', ''));
+					users[tags["channel"]].messagesLeft = users[tags["channel"]].messageInterval;
+				} else if(users[tags["channel"]].countLeft < 1) {
+					return "There are " + users[tags["channel"]].messagesLeft + " messages left till the next !ponder";
+					users[tags["channel"]].countLeft = users[tags["channel"]].countInterval;
+				}
+			} else if(tags["message"] == "!pcount") {
+				if(users[tags["channel"]].countLeft < 1) {
+					return "There are " + Math.max(users[tags["channel"]].messagesLeft, 0) + " messages left till the next !ponder";
+					users[tags["channel"]].countLeft = users[tags["channel"]].countInterval;
+				}
+			} else if(tags["message"] == "!phelp") {
+				if(users[tags["channel"]].helpLeft < 1) {
+					return "A !ponder will be available every 30 messages, and you have one ponder for your user every 24 hours.";
+					users[tags["channel"]].helpLeft = users[tags["channel"]].helpInterval;
+				}
+			}
+			
+			if(users[tags["channel"]].ignores.indexOf(tags["user"]) < 0) {
+				megaHAL.add(tags["message"]);
+			}
+		}
+	};
+	
+	this.pullOptions = function() {
+		
+	};
+	return this;
+};
