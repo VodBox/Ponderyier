eaa426373b5dd930e25288eba3859533debe8e7f
VodBox <dillon@vodbox.io>
2017-03-11 21:39:42 +1300

    Relocate channel loading and adjust configs

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
-var interfaces = {};
+interfaces = {};
 
 fs.readFile('./config.json', 'utf8', function(err, data) {
 	if(err) {
@@ -9,7 +9,41 @@ fs.readFile('./config.json', 'utf8', function(err, data) {
 	} else {
 		var result = JSON.parse(data);
 		for(var key in result) {
-			interfaces[key] = new require("./interfaces/" + key + "/main.js")(result[key]);
+			interfaces[key] = new require("./interfaces/" + key + "/main.js")(result[key], this);
 		}
 	}
 });
+
+fs.readdir('./channels/', function(err, files) {
+	for(var i = 0, l = files.length; i < l; ++i) {
+		fs.readFile('./channels/' + files[i], function(error, response) {
+			var config = JSON.parse(response);
+			for(var key in config) {
+				if(key != "channel") {
+					interfaces[key].addChannel(config[key]);
+				}
+			}
+		});
+	}
+});
+
+isDebugging(function(err, res) {
+	if(err) {
+		console.log('Something went wrong trying to detect debug mode...');
+	} else if(res) {
+		var heapdump = require('heapdump');
+		console.log('debug mode has been detected');
+	}
+});
+
+function isDebugging(cb) {
+	require('net').createServer().on('error', function(err) {
+		if (err.code === 'EADDRINUSE')
+			cb(null, true);
+		else
+			cb(err);
+	}).listen(process.debugPort, function() {
+		this.close();
+		cb(null, false);
+	});
+}
