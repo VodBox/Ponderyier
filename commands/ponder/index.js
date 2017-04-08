e5f6f358e83bd8b18e0e4d277cd65df5d59219b8
wongjoel <wongjoel@users.noreply.github.com>
2017-04-08 20:13:26 +1000

    Split declaration of exports from implementation

--- commands/ponder/index.js
+++ commands/ponder/index.js
@@ -12,6 +12,7 @@ var messageStore = [];
 var badWords = [];
 
 var megaHAL;
+var chatLines;
 
 module.exports = function() {
 	megaHAL = new hal(1);
@@ -24,103 +25,14 @@ module.exports = function() {
 		}
 	});
 
-	//TODO: fill out JSDoc - wongjoel 2017-03-20
-	/**
-	 * Initial setup
-	 * @param  {Object} user -
-	 * @param  {Object} config -
-	 */
-	this.addInstance = function(user, config) {
-		users[user] = {};
-		if(config.unlimitedPonders) {
-			users[user].unlimitedPonders = config.unlimitedPonders;
-		} else {
-			users[user].unlimitedPonders = [];
-		}
-		if(config.resetTime) {
-			users[user].resetTime = config.resetTime;
-		} else {
-			users[user].resetTime = 1600; // GMT
-		}
-		if(config.messageInterval) {
-			users[user].messageInterval = config.messageInterval;
-		} else {
-			users[user].messageInterval = 30;
-		}
-		if(config.helpInterval) {
-			users[user].helpInterval = config.helpInterval;
-		} else {
-			users[user].helpInterval = 15;
-		}
-		if(config.countInterval) {
-			users[user].countInterval = config.countInterval;
-		} else {
-			users[user].countInterval = 10;
-		}
-		if(config.ignores) {
-			users[user].ignores = config.ignores;
-		} else {
-			users[user].ignores = [];
-		}
-
-		users[user].messagesLeft = 0;
-		users[user].helpLeft = 0;
-		users[user].countLeft = 0;
-	};
-
-	//TODO: fill out JSDoc - wongjoel 2017-03-20
-	//TODO: factor out checking for badWords into it's own method? - wongjoel 2017-03-20
-	//TODO: parameterise !phelp response with messageInterval? - wongjoel 2017-03-20
-	/**
-	 * runs the command
-	 * @param  {Object} tags -
-	 * @returns
-	 */
-	this.runCommand = function(tags) {
-		if(users[tags.channel]) {
-			users[tags.channel].messagesLeft--;
-			users[tags.channel].helpLeft--;
-			users[tags.channel].countLeft--;
-			let result;
-			if(tags.message.startsWith('!ponder ')) {
-				if(users[tags.channel].unlimitedPonders.indexOf(tags.user) > -1) {
-					console.log('Unlimited Ponders');
-					return generatePonder(tags.message);
-				} else if(users[tags.channel].messagesLeft < 1) {
-					users[tags.channel].messagesLeft = users[tags.channel].messageInterval;
-					return generatePonder(tags.message);
-				} else if(users[tags.channel].countLeft < 1) {
-					result =  "There are " + Math.max(users[tags.channel].messagesLeft, 0) + " messages left till the next !ponder";
-					users[tags.channel].countLeft = users[tags.channel].countInterval;
-					return result;
-				}
-			} else if(tags.message == "!pcount") {
-				if(users[tags.channel].countLeft < 1) {
-					result =  "There are " + Math.max(users[tags.channel].messagesLeft, 0) + " messages left till the next !ponder";
-					users[tags.channel].countLeft = users[tags.channel].countInterval;
-					return result;
-				}
-			} else if(tags.message == "!phelp") {
-				if(users[tags.channel].helpLeft < 1) {
-					users[tags.channel].helpLeft = users[tags.channel].helpInterval;
-					return "A !ponder will be available every 30 messages, and you have one ponder for your user every 24 hours.";
-				}
-			} else if(tags.message.startsWith("!load ") && tags.user == "dillonea") {
-				loadFromIrc(tags.message.replace("!load ", ""));
-				return "Loading from " + tags.message.replace("!load ", "") + "...";
-			}
-
-			if(users[tags.channel].ignores.indexOf(tags.user) < 0) {
-				megaHAL.add(tags.message);
-			}
-		}
-	};
+	this.addInstance = addInstance;
+	this.runCommand = runCommand;
 
 	/**
 	 * pulls options from the command
 	 * @returns
 	 */
-	this.pullOptions = function() {
+	this.pullOptions = () => {
 		return {
 			"hal": megaHAL
 		};
@@ -130,7 +42,7 @@ module.exports = function() {
 	 * sets options of the command
 	 * @param options - the options to set on the command
 	 */
-	this.setOptions = function(options) {
+	this.setOptions = (options) => {
 		megaHAL = options.hal;
 	};
 
@@ -138,19 +50,127 @@ module.exports = function() {
 	/**
 	 * exit
 	 */
-	this.exit = function() {
+	this.exit = () => {
 		return true;
 	};
 	return this;
 };
-var chatLines;
 
+//TODO: fill out JSDoc - wongjoel 2017-03-20
+/**
+ * Initial setup
+ * @param  {Object} user -
+ * @param  {Object} config -
+ */
+function addInstance(user, config) {
+	console.log("add instance: " + user);
+	console.log("add instance config: ");
+	console.log(config);
+	console.log("users before: ");
+	console.log(users);
+	let inputUser = {};
+	if(config.unlimitedPonders) {
+		inputUser.unlimitedPonders = config.unlimitedPonders;
+	} else {
+		inputUser.unlimitedPonders = [];
+	}
+	if(config.resetTime) {
+		inputUser.resetTime = config.resetTime;
+	} else {
+		inputUser.resetTime = 1600; // GMT
+	}
+	if(config.messageInterval) {
+		inputUser.messageInterval = config.messageInterval;
+	} else {
+		inputUser.messageInterval = 30;
+	}
+	if(config.helpInterval) {
+		inputUser.helpInterval = config.helpInterval;
+	} else {
+		inputUser.helpInterval = 15;
+	}
+	if(config.countInterval) {
+		inputUser.countInterval = config.countInterval;
+	} else {
+		inputUser.countInterval = 10;
+	}
+	if(config.ignores) {
+		inputUser.ignores = config.ignores;
+	} else {
+		inputUser.ignores = [];
+	}
+
+	inputUser.messagesLeft = 0;
+	inputUser.helpLeft = 0;
+	inputUser.countLeft = 0;
+
+	users[user] = inputUser;
+
+	console.log("users after: ");
+	console.log(users);
+};
+
+
+//TODO: fill out JSDoc - wongjoel 2017-03-20
+//TODO: parameterise !phelp response with messageInterval? - wongjoel 2017-03-20
+/**
+ * runs the command
+ * @param  {Object} tags -
+ * @returns
+ */
+function runCommand(tags) {
+	let channel = tags.channel;
+	let message = tags.message;
+	if(users[tags.channel]) {
+		users[tags.channel].messagesLeft--;
+		users[tags.channel].helpLeft--;
+		users[tags.channel].countLeft--;
+		let result;
+		if(tags.message.startsWith('!ponder ')) {
+			if(users[tags.channel].unlimitedPonders.indexOf(tags.user) > -1) {
+				console.log('Unlimited Ponders');
+				return generatePonder(tags.message);
+			} else if(users[tags.channel].messagesLeft < 1) {
+				users[tags.channel].messagesLeft = users[tags.channel].messageInterval;
+				return generatePonder(tags.message);
+			} else if(users[tags.channel].countLeft < 1) {
+				result =  "There are " + Math.max(users[tags.channel].messagesLeft, 0) + " messages left till the next !ponder";
+				users[tags.channel].countLeft = users[tags.channel].countInterval;
+				return result;
+			}
+		} else if(tags.message == "!pcount") {
+			if(users[tags.channel].countLeft < 1) {
+				result =  "There are " + Math.max(users[tags.channel].messagesLeft, 0) + " messages left till the next !ponder";
+				users[tags.channel].countLeft = users[tags.channel].countInterval;
+				return result;
+			}
+		} else if(tags.message == "!phelp") {
+			if(users[tags.channel].helpLeft < 1) {
+				users[tags.channel].helpLeft = users[tags.channel].helpInterval;
+				return "A !ponder will be available every 30 messages, and you have one ponder for your user every 24 hours.";
+			}
+		} else if(tags.message.startsWith("!load ") && tags.user == "dillonea") {
+			loadFromIrc(tags.message.replace("!load ", ""));
+			return "Loading from " + tags.message.replace("!load ", "") + "...";
+		}
+
+		if(users[tags.channel].ignores.indexOf(tags.user) < 0) {
+			megaHAL.add(tags.message);
+		}
+	}
+};
+
+/**
+ * load from IRC
+ * @param file -
+ */
 function generatePonder(message) {
 	var valid = false;
 	var attempts = 0;
 	var ponder;
 	while(!valid) {
 		ponder = megaHAL.getReplyFromSentence(message.replace('/\!ponder /', ''));
+		console.log(ponder);
 		valid = true;
 		for(var i = 0, l = badWords.length; i < l; ++i) {
 			if(ponder.includes(badWords[i])) {
@@ -166,7 +186,6 @@ function generatePonder(message) {
 	return ponder;
 }
 
-//TODO: fill out JSDoc - wongjoel 2017-03-20
 /**
  * load from IRC
  * @param file -
