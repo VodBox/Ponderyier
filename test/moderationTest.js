d0d43e9faceb37cf0ca39d5c7dddc356da4cca7d
wongjoel <wongjoel@users.noreply.github.com>
2017-05-06 20:04:01 +1000

    Instantiate moderation module in test

--- test/moderationTest.js
+++ test/moderationTest.js
@@ -2,7 +2,28 @@
  * Tests the moderation command
  */
 
+const assert = require('assert');
+const moderationModule = require('../commands/moderation/index.js');
+var moderation = new moderationModule();
+
 describe('Ponder', function () {
+	before("Instantiate Moderation Module", function () {
+		moderation = new moderationModule();
+		const config = {
+				symbols: true,
+				caps: true,
+				emotes: true,
+				badWords: true,
+				spam: true,
+				symbolProportion: 0.5,
+				capsProportion: 0.8,
+				emoteTolerance: 5,
+				customBadWords: [],
+				spamTolerance: 3, // per second
+				verboseChat: true
+		};
+		moderation.addInstance('testChat', config);
+	});
 
 	describe("Caps", function () {
 		it("should bop too many caps");
