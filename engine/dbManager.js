8a7fbaa2920b1f60a817511acd44307e575dc5cb
VodBox <dillon@vodbox.io>
2017-07-17 11:39:58 +1200

    Add custom datastore manager class

--- engine/dbManager.js
+++ engine/dbManager.js
@@ -1,5 +1,169 @@
 /**Database Manager */
-const Datastore = require('nedb');
+const fs = require("fs");
+
+module.exports = function(filepath) {
+	if(!filepath) {
+		filepath = "main.json";
+	}
+	this.filepath = filepath;
+	let self = this;
+	parseFile(filepath, self, function() {
+		if(self.store === undefined) {
+			self.store = {};
+		}
+	});
+
+	this.createObject = createObject;
+	this.createArray = createArray;
+
+	this.getKey = getKey;
+	this.setKey = setKey;
+	this.removeKey = removeKey;
+};
+
+function createObject(filepath, callback) {
+	fs.writeFile("../db/" + filepath, "{}", function(err) {
+		if(err) {
+			callback({
+				"error": true,
+				"type": "reference",
+				"filepath": filepath
+			});
+		} else {
+			callback({
+				"error": false,
+				"type": "reference",
+				"filepath": filepath
+			});
+		}
+	});
+}
+
+function createArray(filepath, callback) {
+	fs.writeFile("../db/" + filepath, "[]", function(err) {
+		if(err) {
+			callback({
+				"error": true,
+				"type": "reference",
+				"filepath": filepath
+			});
+		} else {
+			callback({
+				"error": false,
+				"type": "reference",
+				"filepath": filepath
+			});
+		}
+	});
+}
+
+function getKey(key) {
+	return this.store[key];
+}
+
+function setKey(key, value) {
+	if(typeof value !== "function") {
+		if(value !== undefined && value !== null) {
+			if(value.type && value.type == "reference" && value.filepath) {
+				if(!value.error) {
+					this.store[key] = new module.exports(value.filepath);
+				}
+			} else {
+				this.store[key] = value;
+			}
+		}
+	}
+	save(this);
+}
+
+function removeKey(key) {
+	delete this.store[key];
+}
+
+function save(self) {
+	fs.writeFile('../db/' + self.filepath, JSON.stringify(self.store, function(key, value) {
+		if(value instanceof module.exports) {
+			return {
+				type: "reference",
+				filepath: value.filepath
+			};
+		}
+		return value;
+	}, 2), function(err) {
+		if(err) {
+			console.error(err);
+		}
+	});
+}
+
+function parseFile(filepath, self, callback) {
+	fs.readFile("../db/" + filepath, function(err, data) {
+		if(err) {
+			console.error(err);
+		} else {
+			try {
+				self.store = JSON.parse(data);
+				if(typeof self.store === "object" && !Array.isArray(self.store)) {
+					connectRefsObj(self.store);
+				} else if(typeof self.store === "object") {
+					connectRefsArr(self.store);
+				}
+			} catch(e) {
+				self.store = undefined;
+			}
+			callback();
+		}
+	});
+}
+
+function connectRefsObj(obj) {
+	for(let key in obj) {
+		if(typeof obj[key] === "object" && !Array.isArray(obj[key])) {
+			if(obj[key].type && obj[key].type == "reference" && obj[key].filepath) {
+				obj[key] = new module.exports(obj[key].filepath);
+			} else {
+				console.log('beforeobj');
+				connectRefsObj(obj[key]);
+			}
+		} else if(typeof obj[key] === "object") {
+			connectRefsArr(obj[key]);
+		}
+	}
+}
+
+function connectRefsArr(arr) {
+	for(let index in arr) {
+		let item = arr[index];
+		console.log(item);
+		if(typeof item === "object" && !Array.isArray(item)) {
+			if(item.type && item.type == "reference" && item.filepath) {
+				arr[index] = new module.exports(item.filepath);
+			} else {
+				connectRefsObj(item);
+			}
+		} else if(typeof item === "object") {
+			connectRefsArr(item);
+		}
+	}
+}
+
+/*let test = new module.exports();
+setTimeout(function() {
+	test.createObject("test.json", function(res) {
+		test.setKey("test", res);
+		test.createArray("array.json", function(res2) {
+			test.getKey("ref").setKey("array", res2);
+			test.getKey("list")[2].setKey("thing", true);
+			console.log(test.getKey("ref").getKey("array"));
+			setTimeout(function () {
+				console.log(test.getKey("ref").getKey("array").store);
+				test.getKey("ref").getKey("array").setKey(0, "test");
+			}, 1000);
+		});
+	});
+}, 5000);*/
+
+/**const Datastore = require('nedb');
 var db;
 
 module.exports = function() {
@@ -85,4 +249,4 @@ addChat("test", function() {
 			console.log(data);
 		});
 	});
-});
+});**/
